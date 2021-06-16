b926fbf3bc47101d3294e625ad7e0c01deaac17c
DanielZlotin <zlotindaniel@gmail.com>
2021-06-16 11:24:01 +0300

    fixing ts

--- handler.ts
+++ handler.ts
@@ -1,27 +1,27 @@
 import { FileStorage } from "./src/storage";
 import { onGet, onSchedule } from "./src/main";
 
-const storage = new FileStorage(process.env.HOME_DIR);
+const storage = new FileStorage(process.env.HOME_DIR!!);
 
 // handlers
 
-async function reader(event, context) {
+async function reader(event: any, context: any) {
   return success(await onGet(storage, event.pathParameters.param));
 }
 
-async function writer(event, context) {
+async function writer(event: any, context: any) {
   await onSchedule(storage);
   return success("OK");
 }
 
-function success(result) {
+function success(result: any) {
   return {
     statusCode: 200,
     body: JSON.stringify(result, null, 2),
   };
 }
 
-async function catchErrors(event, context) {
+async function catchErrors(this: any, event: any, context: any) {
   try {
     return await this(event, context);
   } catch (err) {
